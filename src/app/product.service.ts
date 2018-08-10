026bfa0cd72badf84842b47a6dff53ac22e126fd
Jelmer <jelmerhss@gmail.com>
2018-08-10 10:31:59 +0200

    Product Service update

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from '../../node_modules/rxjs/Observable';
-import 'rxjs';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -10,7 +9,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number): Observable<any> {
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl)
       .map((response) => response.json());
   }
